1df0b5785dba79a0e47ae8fa7c0a1e2af0b49a27
Christian <sghradev@gmail.com>
2019-12-08 18:28:07 +0000

    added tearDown for transformer test

--- tests/test_transformer.py
+++ tests/test_transformer.py
@@ -1,3 +1,5 @@
+import glob
+import os
 from unittest import TestCase
 
 from transform import Transformer
@@ -7,6 +9,11 @@ class TestTransformer(TestCase):
 
     def setUp(self):
         self.transformer = Transformer()
+        self.project_dir = os.path.abspath(__file__ + "/../../")
+
+    def tearDown(self):
+        for f in glob.glob(self.project_dir + "/resources/*.parquet.gzip"):
+            os.remove(f)
 
     def test___init__(self):
         result = self.transformer.find_hottest_day()
